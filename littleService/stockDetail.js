0e14c4bd71eaa0f6319feaeee5004f1aa31f57e8
xiang19890319@gmail.com <xiang19890319@gmail.com>
2017-06-07 23:05:01 +0800

    urls

--- littleService/stockDetail.js
+++ littleService/stockDetail.js
@@ -1,1 +1,4 @@
-var url = `https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=sh601016&step=3&start=&count=160&fq_type=front&timestamp=1496846898371`;
+var urlTools = `https://gupiao.baidu.com/api/stocks/stockdaybar?from=pc&os_ver=1&cuid=xxx&vv=100&format=json&stock_code=sh601016&step=3&start=&count=160&fq_type=front&timestamp=1496846898371`;
+var urlStocksPage1 = `http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=40&sort=symbol&asc=1&node=hs_a&symbol=&_s_r_a=init`;
+var urlStocksPage80 = `http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=80&num=40&sort=symbol&asc=1&node=hs_a&symbol=&_s_r_a=page`;
+
